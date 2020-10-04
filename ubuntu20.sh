3c1fceabcdde3bdf2fe4bdc3c18aa946659cf000
Per Nilsson Lundberg <pernlundberg@gmail.com>
2020-10-04 22:37:41 +0200

    Added start of ubuntu 20 script

---
+++ ubuntu20.sh
@@ -0,0 +1,11 @@
+sudo apt install i3-wm
+sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake i3status suckless-tools feh i3lock rofi curl wget compton libxcb-shape0-dev
+sudo apt-get install -y git g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf
+sudo apt install vim
+sudo apt-get install libtool
+ssh-keygen -t rsa -b 4096 -C "pernlundberg@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub
+
